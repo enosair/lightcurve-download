262891742827f1d32f5fa5cc6a7aa158d2c2151f
enosair <qinqingnew@gmail.com>
2014-10-31 13:50:48 -0500

    example to load json and plot flux

---
+++ example.py
@@ -0,0 +1,48 @@
+# example to load and plot data from json
+# ---------------------------------------
+
+import json
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+
+    if len(sys.argv) == 2:
+        jf = os.path.abspath(sys.argv[1])  # source json file
+    else:                 
+        sys.exit('Usage: python process.py <source json file>')
+
+    # load data
+    lc = json.load(open(jf, 'r'))
+
+    # plot flux for the first two lightcurves
+    i = 0
+    for k, v in lc.iteritems():
+        i += 1
+        if i > 2:
+            break
+        else:
+            t   = np.array(v['TIME'])
+            raw = np.array(v['SAP_FLUX'])              # raw flux
+            pdc = np.array(v['PDCSAP_FLUX'])           # PDC pipeline processed flux
+            
+
+            plt.subplot(2, 2, i)
+            plt.plot(t[~np.isnan(raw)], raw[~np.isnan(raw)], 'k.') # remove nan
+            plt.title(k)
+            plt.xlabel('time')
+            plt.xlabel('raw flux')
+
+            plt.subplot(2, 2, i+2)
+            plt.plot(t[~np.isnan(raw)], pdc[~np.isnan(raw)], 'r.') # remove nan
+            plt.title(k)
+            plt.xlabel('time')
+            plt.xlabel('PDC corrected flux')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
