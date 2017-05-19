c215bc8af3369a39340cba13f69f4a5dd8845fa7
qinqing <qinqingnew@gmail.com>
2017-05-18 23:37:57 -0500

    u

---
+++ conf/wget_conf.sh
@@ -0,0 +1,4 @@
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0007/000757450/kplr000757450-2013131215648_llc.fits
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0017/001718189/kplr001718189-2013131215648_llc.fits
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0018/001871056/kplr001871056-2013131215648_llc.fits
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0021/002165002/kplr002165002-2013131215648_llc.fits
---
+++ fp/wget_fp.sh
@@ -0,0 +1,4 @@
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0008/000892772/kplr000892772-2013131215648_llc.fits
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0010/001026957/kplr001026957-2013131215648_llc.fits
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0010/001027438/kplr001027438-2013131215648_llc.fits
+wget -q http://archive.stsci.edu/missions/kepler/lightcurves/0011/001161949/kplr001161949-2013131215648_llc.fits
